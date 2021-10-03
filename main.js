8c9439fbc1cd5789709b30847f9791163af877ed
Pathawee Somsak <pathawee.s@wenext.net>
2021-10-03 17:52:05 +0700

    create buffer-copy, buffer-concate

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-buf1 = new Buffer(256);
+console.log(process.memoryUsage())
 
-buf1.write("hello world");
+buff1 = new Buffer(256);
+buff1.write("hello world");
+console.log("buf1 > ", buff1.toString())
 
-// console.log(buf1);
-console.log("buf1 > ", buf1.toString())
+buff2 = new Buffer(256);
+buff2.write("!!!!!");
+console.log("buff2 > ", buff2.toString())
 
-buf2 = new Buffer(256);
+var buff3 = Buffer.concat([buff1,buff2]);
+console.log("buff3 > ", buff3.toString())
 
-buf2.write("!!!!!");
+var buff4 = new Buffer(512);
+buff3.copy(buff4);
+console.log("buff4 > ", buff4.toString())
 
-// console.log(buf1);
-console.log("buf2 > ", buf2.toString())
-
-var buff3 = Buffer.concat([buf1,buf2]);
-console.log(buff3.toString())
+console.log(process.memoryUsage())
 
